4a572ef3b16d9cd0f620bbba40795f8a38a5b617
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2021-04-15 17:31:54 -0300

    Aula 62
    Aceitando apenas numeros no input
    Recolhendo o teclado ao clicar na tela com o "TouchableWithoutFeedback"

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native';
 
 import Card from '../components/Card'
 import Colors from '../constants/colors'
 import Input from '../components/Input'
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Começar um novo jogo!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Selecione um numero</Text>
-                <Input blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} style={styles.input}></Input>
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}><Button title="Resetar" onPress={() => { }} color={Colors.accent} /></View>
-                    <View style={styles.button}><Button title="Confirmar" onPress={() => { }} color={Colors.primary} /></View>
-                </View>
-            </Card>
-        </View>
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
+
+            <View style={styles.screen}>
+                <Text style={styles.title}>Começar um novo jogo!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Selecione um numero</Text>
+                    <Input
+                        blurOnSubmit
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        keyboardType="number-pad"
+                        maxLength={2}
+                        style={styles.input}
+                        onChangeText={numberInputHandler}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}><Button title="Resetar" onPress={() => { }} color={Colors.accent} /></View>
+                        <View style={styles.button}><Button title="Confirmar" onPress={() => { }} color={Colors.primary} /></View>
+                    </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
@@ -36,7 +64,7 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15
-    },    
+    },
     inputContainer: {
         width: 300,
         maxWidth: '80%',
