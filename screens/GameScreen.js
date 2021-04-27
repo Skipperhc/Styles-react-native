30843b0b153bbd226b408cdab880d8ed6c603fb1
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2021-04-27 15:19:07 -0300

    Listando tentativas anteriores

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import { View, Text, StyleSheet, Button, Alert, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
@@ -19,10 +19,9 @@ const generateRandomBetween = (min, max, exclude) => {
 }
 
 const GameScreen = props => {
-
-    const [rounds, setRounds] = useState(0);
-    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
-
+    const initialGuess = generateRandomBetween(1, 100, props.userChoice);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    const [pastGuesses, setPastGuesses] = useState([initialGuess]);
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
@@ -30,7 +29,7 @@ const GameScreen = props => {
 
     useEffect(() => {
         if (currentGuess === userChoice) {
-            onGameOver(rounds);
+            onGameOver(pastGuesses.length);
         }
     }, [currentGuess, userChoice, onGameOver]);
 
@@ -46,11 +45,11 @@ const GameScreen = props => {
         if (direction === 'menor') {
             currentHigh.current = currentGuess;
         } else {
-            currentLow.current = currentGuess;
+            currentLow.current = currentGuess + 1;
         }
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
         setCurrentGuess(nextNumber);
-        setRounds(rounds => rounds + 1)
+        setPastGuesses(curPastGuesses => [nextNumber, ...curPastGuesses]);
     }
 
     return (
@@ -76,6 +75,14 @@ const GameScreen = props => {
                 </MainButton>
 
             </Card>
+            <ScrollView>
+                {pastGuesses.map(guess =>
+                    <View key={guess}>
+                        <Text>
+                            {guess}
+                        </Text>
+                    </View>)}
+            </ScrollView>
         </View>
     );
 
