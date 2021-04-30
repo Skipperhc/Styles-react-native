8018f46baa6c5e804fd7ae487eeefab9bc843614
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2021-04-30 15:42:30 -0300

    Usando os melhores efeitos para cada plataforma

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,39 @@
 import React from 'react'
-import {View, Text, StyleSheet, Platform } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 
 import TitleText from '../components/TitleText'
 import Colors from '../constants/colors';
 
 const Header = props => {
     return (
-        <View style={styles.header}>
+        <View style={{
+            ...styles.headerBase,
+            ...Platform.select({
+                ios: styles.headerIos,
+                android: styles.headerAndroid
+            })
+        }}>
             <TitleText style={styles.title}>{props.title}</TitleText>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 30,
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
         alignItems: 'center',
         justifyContent: 'center',
-        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+
+    },
+    headerIos: {
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary
     },
     title: {
         color: Platform.OS === 'ios' ? Colors.primary : 'white'
